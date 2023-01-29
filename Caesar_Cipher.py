acc51caa6ceb2dd859739f6ea92730499f0430ee
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2023-01-29 16:48:55 +0100

    made it a bit prettier

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,35 +1,39 @@
 def encrypt(plain_text: str, shift: int) -> str:
     cipher_text = ""
     for char in plain_text:
-        if char.isalpha():
-            if char.isupper():
-                shift_char = chr((ord(char) + shift - 65) % 26 + 65)
-            else:
-                shift_char = chr((ord(char) + shift - 97) % 26 + 97)
-            cipher_text += shift_char
-        else:
+        if not char.isalpha():
             cipher_text += char
+            continue
+        if char.isupper():
+            shift_char = chr((ord(char) + shift - 65) % 26 + 65)
+        else:
+            shift_char = chr((ord(char) + shift - 97) % 26 + 97)
+        cipher_text += shift_char
+
     return cipher_text
 
 
 def decrypt(cipher_text: str, shift: int) -> str:
     plain_text = ""
     for char in cipher_text:
-        if char.isalpha():
-            if char.isupper():
-                shift_char = chr((ord(char) - shift - 65) % 26 + 65)
-            else:
-                shift_char = chr((ord(char) - shift - 97) % 26 + 97)
-            plain_text += shift_char
+        if not char.isalpha():
+            cipher_text += char
+            continue
+        if char.isupper():
+            shift_char = chr((ord(char) - shift - 65) % 26 + 65)
         else:
-            plain_text += char
+            shift_char = chr((ord(char) - shift - 97) % 26 + 97)
+        plain_text += shift_char
+
     return plain_text
 
 
+def decrypt_all(cipher_text: str) -> None:
+    for i in range(26):
+        decrypted = encrypt(cipher_text, i)
+        print(f"{i}:\t{decrypted}")
+
+
 if __name__ == '__main__':
-    text = "Hello World!"
-    shift = 10
-    encrypted_text = encrypt(plain_text=text, shift=shift)
-    print(encrypted_text)
-    print(encrypt(encrypted_text, shift=26 - shift))
-    print(decrypt(encrypted_text, shift=shift))
+    text = "RU SQHUVKB MXQJ AYDT EV UDSHOFJYED JE KIU JXUHU QHU Q BEJ EV RQT SYFXUHI EKJ JXUHU"
+    decrypt_all(text)

9491b7ede8326ebf60fa86d2da10b881acfc83a9
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2023-03-06 13:02:21 +0100

    added greatest common divider

---
+++ Greatest_Common_Divider.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 09:10:21 2021
+
+@author: madsn
+"""
+#imported libraries numpy because reason pandas because looks nice
+import numpy as np
+import pandas as pd
+
+#Euclidisk extended allegoritme
+#made by Mads Legard Nielsen
+#input the a and b from you assignment thingy
+#Euclidisk extended allegoritme
+#made by Mads Legard Nielsen
+def euclidall(a,b):
+    a=np.abs(a)
+    b=np.abs(b)
+    i=[-1,0]
+    r_i=np.array([a,b],dtype=np.int32)
+    q_i=np.array([0,0],dtype=np.int32)
+    s_i=np.array([1,0],dtype=np.int32)
+    t_i=np.array([0,1],dtype=np.int32)
+    while (b!=0):
+        r = a%b
+        q = int(a/b)
+        q_i=np.append(q_i,q)
+        s = s_i[-2]-q_i[-1]*s_i[-1]
+        t = t_i[-2]-q_i[-1]*t_i[-1]
+        a= b
+        b=r
+        i=np.append(i,(i[-1])+1)
+        r_i=np.append(r_i,r)
+        s_i=np.append(s_i,s)
+        t_i=np.append(t_i,t)
+        d={"ri":r_i,"qi":q_i,"si":s_i,"ti":t_i}
+        df=pd.DataFrame(data=d,index=i)
+        latex=df.to_latex()
+    return latex
+
+print(euclidall(7, 9))
